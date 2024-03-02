31a26b1a436a450cee4c0b6f420f653f54dcf17c
ION <thegoshagames@gmail.com>
2024-03-02 19:35:31 +0300

    Cookie

--- src/components/AuthDialog.tsx
+++ src/components/AuthDialog.tsx
@@ -1,133 +1,132 @@
 import { Button } from "@/components/ui/button";
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogFooter,
-    DialogHeader,
-    DialogTitle,
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { getCookieValue, removeCookieValue } from "@/lib/utils";
 import { DialogTrigger } from "@radix-ui/react-dialog";
 import axios from "axios";
 import { useState } from "react";
-import { useCookies } from "react-cookie";
+
 export interface IToken {
-    accessToken: string;
-    refreshToken: string;
+  accessToken: string;
+  refreshToken: string;
 }
 
 export function AuthDialog() {
-    const [user, setUser] = useState<{ phone: string; password: string }>({
-        phone: "+79000000000",
-        password: "string",
+  const [user, setUser] = useState<{ phone: string; password: string }>({
+    phone: "+79000000000",
+    password: "string",
+  });
+
+  const [open, setOpen] = useState<boolean>(true);
+  const accessToken = getCookieValue("access_token");
+  const refreshToken = getCookieValue("refresh_token");
+  //   const [cookies, setCookie, removeCookie] = useCookies([
+  //     "access_token",
+  //     "refresh_token",
+  //   ]);
+
+  const expires = new Date();
+  expires.setTime(expires.getTime() + 12 * 60);
+
+  const handleOnClick = async () => {
+    const res = await axios.post<IToken>(
+      `${import.meta.env.VITE_API}/auth`,
+      user
+    );
+
+    if (res.status === 201) {
+      document.cookie = `access_token=${res.data.accessToken}; Max-Age=86400`;
+      document.cookie = `refresh_token=${res.data.refreshToken}; Max-Age=86400`;
+      //   setCookie("access_token", res.data.accessToken, {
+      //     expires,
+      //   });
+      //   setCookie("refresh_token", res.data.refreshToken, {
+      //     expires,
+      //   });
+      setOpen(false);
+    }
+  };
+
+  const clearValues = async () => {
+    if (!accessToken) return;
+    const res = await axios.delete(`${import.meta.env.VITE_API}/auth/logout`, {
+      data: { refresh_token: refreshToken },
     });
 
-    const [open, setOpen] = useState<boolean>(true);
-    const [cookies, setCookie, removeCookie] = useCookies([
-        "access_token",
-        "refresh_token",
-    ]);
+    if (res.status === 200) {
+      setOpen(true);
 
-    const expires = new Date();
-    expires.setTime(expires.getTime() + 12 * 60);
+      removeCookieValue("access_token");
+      removeCookieValue("refresh_token");
+    }
+  };
 
-    const handleOnClick = async () => {
-        const res = await axios.post<IToken>(
-            `${import.meta.env.VITE_API}/auth`,
-            user
-        );
+  return (
+    <Dialog
+      open={open}
+      onOpenChange={setOpen}
+      defaultOpen={!accessToken && !refreshToken && true}
+    >
+      <DialogTrigger asChild>
+        <Button onClick={clearValues}>{accessToken ? "Выйти" : "Войти"}</Button>
+      </DialogTrigger>
 
-        if (res.status === 201) {
-            setCookie("access_token", res.data.accessToken, {
-                path: "/",
-                expires,
-            });
-            setCookie("refresh_token", res.data.refreshToken, {
-                path: "/",
-                expires,
-            });
-            setOpen(false);
-        }
-    };
-
-    const clearValues = async () => {
-        if (!cookies.access_token) return;
-        const res = await axios.delete(
-            `${import.meta.env.VITE_API}/auth/logout`,
-            { data: { refresh_token: cookies.refresh_token } }
-        );
-
-        if (res.status === 200) {
-            setOpen(true);
-
-            removeCookie("access_token");
-            removeCookie("refresh_token");
-        }
-    };
-
-    return (
-        <Dialog
-            open={open}
-            onOpenChange={setOpen}
-            defaultOpen={!cookies && true}
-        >
-            <DialogTrigger asChild>
-                <Button onClick={clearValues}>
-                    {cookies.access_token ? "Выйти" : "Войти"}
-                </Button>
-            </DialogTrigger>
-
-            <DialogContent className="sm:max-w-[425px]">
-                <DialogHeader>
-                    <DialogTitle>Авторизация</DialogTitle>
-                    <DialogDescription>
-                        Чтобы получить рейтинг, нужно авторизоваться для
-                        получения доступа
-                    </DialogDescription>
-                </DialogHeader>
-                <div className="grid gap-4 py-4">
-                    <div className="grid grid-cols-4 items-center gap-4">
-                        <Label htmlFor="phone" className="text-right">
-                            Телефон
-                        </Label>
-                        <Input
-                            id="phone"
-                            placeholder={user.phone}
-                            onChange={(e) =>
-                                setUser({
-                                    ...user,
-                                    phone: e.currentTarget.value,
-                                })
-                            }
-                            className="col-span-3"
-                        />
-                    </div>
-                    <div className="grid grid-cols-4 items-center gap-4">
-                        <Label htmlFor="password" className="text-right">
-                            Пароль
-                        </Label>
-                        <Input
-                            id="password"
-                            placeholder={user.password}
-                            type="password"
-                            onChange={(e) =>
-                                setUser({
-                                    ...user,
-                                    password: e.currentTarget.value,
-                                })
-                            }
-                            className="col-span-3"
-                        />
-                    </div>
-                </div>
-                <DialogFooter>
-                    <Button onClick={handleOnClick} type="submit">
-                        Войти
-                    </Button>
-                </DialogFooter>
-            </DialogContent>
-        </Dialog>
-    );
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Авторизация</DialogTitle>
+          <DialogDescription>
+            Чтобы получить рейтинг, нужно авторизоваться для получения доступа
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="phone" className="text-right">
+              Телефон
+            </Label>
+            <Input
+              id="phone"
+              placeholder={user.phone}
+              onChange={(e) =>
+                setUser({
+                  ...user,
+                  phone: e.currentTarget.value,
+                })
+              }
+              className="col-span-3"
+            />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="password" className="text-right">
+              Пароль
+            </Label>
+            <Input
+              id="password"
+              placeholder={user.password}
+              type="password"
+              onChange={(e) =>
+                setUser({
+                  ...user,
+                  password: e.currentTarget.value,
+                })
+              }
+              className="col-span-3"
+            />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button onClick={handleOnClick} type="submit">
+            Войти
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,9 +1,9 @@
-import { type ClassValue, clsx } from "clsx"
+import { type ClassValue, clsx } from "clsx";
 
-import { twMerge } from "tailwind-merge"
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
 export const formatInitials = (
@@ -11,14 +11,23 @@ export const formatInitials = (
   lastName: string,
   patronymic: string
 ) => {
-  const str = `${lastName} ${firstName} ${patronymic}`
+  const str = `${lastName} ${firstName} ${patronymic}`;
 
   return str
-      .split(/\s+/)
-      .map((w, i) => (i ? w.substring(0, 1).toUpperCase() + '.' : w))
-      .join(' ')
-}
+    .split(/\s+/)
+    .map((w, i) => (i ? w.substring(0, 1).toUpperCase() + "." : w))
+    .join(" ");
+};
 
+export const getCookieValue = (key: string) => {
+  const cookieValue = document.cookie
+    .split("; ")
+    .find((row) => row.startsWith(`${key}=`))
+    ?.split("=")[1];
 
+  return cookieValue;
+};
 
-
+export const removeCookieValue = (key: string) => {
+  document.cookie = `${key}=; Max-Age=-1`;
+};
