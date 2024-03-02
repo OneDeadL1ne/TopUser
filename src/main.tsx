34d2aec98cf7ca0d113e911d44196203bf792486
ION <thegoshagames@gmail.com>
2024-03-02 22:18:46 +0300

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { CookiesProvider } from "react-cookie";
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <CookiesProvider>
-                <App />
-            </CookiesProvider>
-        </BrowserRouter>
-    </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
